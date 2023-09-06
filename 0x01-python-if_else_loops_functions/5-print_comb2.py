fa86d63dc5e776de962feab4bfc6539f6b360cf8
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-09-06 03:14:32 +0300

    test

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(0, 10):
-        if (i == 9 and j == 9):
-            print(f"{i}{j}")
-        elif (j == 9):
-            print(f"{i}{j}, ", end='')
+        if (j == 9 and i != 9):
+            print("{:d}{:d}, ".format(i, j), end='')
             i += 1
             continue
-        print(f"{i}{j}, ", end='')
+        print("{:d}{:d}".format(i, j), end='\n' if i == 9 and j == 9 else ", ")
