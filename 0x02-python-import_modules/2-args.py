91168cf667574a657a2870bc682aba72e6222023
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-09-08 03:36:53 +0300

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     size = len(argv) - 1
 
     if (size == 0):
-        print("{} arguments.".format(size))
+        print("{:d} arguments.".format(size))
     else:
         arg = argv[1:]
-        print("{} arguments:".format(size))
+        print("{:d} arguments:".format(size))
         for i in range(size):
-            print("{}: {}".format(i + 1, arg[i]))
+            print("{:d}: {:s}".format(i + 1, arg[i]))
