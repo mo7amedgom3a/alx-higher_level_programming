13be050775db6155a1f68e50d0349a561770af74
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-09-14 05:17:32 +0300

    test

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None or len(a_dictionary) == 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
     current_high_score = 0
