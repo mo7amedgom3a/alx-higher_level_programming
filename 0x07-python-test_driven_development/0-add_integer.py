2cf5425519d8d834f158d495cb55d1029fda9bee
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-10-03 23:36:30 -0700

    dhkvj

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,116 +1,26 @@
-Test cases for 0-add_integer module
-===================================
+#!/usr/bin/python3
+"""
 
-The 0-add_integer module takes two integers or floats as arguments and returns their sum
+This module has one function that adds up 2 integers
 
-Usage
-=====
+"""
 
-Importing function from the module:
-    >>> add_integer = __import__('0-add_integer').add_integer
 
-Adding two positive integers (a = 1 and b = 1)
-    >>> add_integer(1, 1)
-    2
+def add_integer(a, b=98):
+    """Return the sum of two integers or floats as integers
 
-Adding one positive and one negative integer (a = 10 and b = -4)
-    >>> add_integer(10, -4)
-    6
+    Args:
+        a: first argument
+        b: second argument
 
-Adding two negative integers (a = -10 and b = -4)
-    >>> add_integer(-10, -4)
-    -14
+    Returns:
+        Sum of the two arguments
 
-Adding two integers with the second being the same as the default value of b (a = 2 and b = 98)
-    >>> add_integer(2)
-    100
-
-Adding a positive float and a positive integer (a = 2.5 and b = 5)
-    >>> add_integer(2.5, 5)
-    7
-
-Adding a float and an integer where the integer is equal to the default value of b (a = 2.5 and b = 98)
-    >>> add_integer(2.5)
-    100
-
-Adding a positive float and a negative integer (a = 10.5 and b = -2)
-	    >>> add_integer(10.5, -2)
-	    8
-
-Adding two negative floats (a = -10.5 and b = -2)
-	    >>> add_integer(-10.5, -2)
-	    -12
-
-Adding an integer number and a string (a = 4 and b = "School")
-    >>> add_integer(4, "School")
-    Traceback (most recent call last):
-	      ...
-    TypeError: b must be an integer
-
-Passing no argument to the function
-    >>> add_integer(None)
-    Traceback (most recent call last):
-	      ...
-    TypeError: a must be an integer
-
-Adding a letter and a number (a = 'c' and b = 1)
-    >>> add_integer('c', 1)
-    Traceback (most recent call last):
-              ...
-    TypeError: a must be an integer
-
-Adding two letters (a = 'c' and b = 'm')
-    >>> add_integer('c', 'm')
-    Traceback (most recent call last):
-              ...
-    TypeError: a must be an integer
-
-Adding a tuple
-    >>> add_integer((1, 1))
-    Traceback (most recent call last):
-              ...
-    TypeError: a must be an integer
-
-Adding a number and a list
-    >>> add_integer(123, [])
-    Traceback (most recent call last):
-	      ...
-    TypeError: b must be an integer
-
-Passing a only one string
-    >>> add_integer("Hello")
-    Traceback (most recent call last):
-	      ...
-    TypeError: a must be an integer
-
-Adding two float numbers (a = 1.2 and b = 4.2)
-    >>> add_integer(1.2, 4.2)
-    5
-
-Case Overflow:
-
-    >>> add_integer(float('inf'), 0)
-    Traceback (most recent call last):
-    	      ...
-    OverflowError: cannot convert float infinity to integer
-
-Case Overflow 2:
-
-    >>> add_integer(float('inf'), float('-inf'))
-    Traceback (most recent call last):
-    	      ...
-    OverflowError: cannot convert float infinity to integer
-
-Case NaN 1:
-
-    >>> add_integer(0, float('nan'))
-    Traceback (most recent call last):
-    	      ...
-    ValueError: cannot convert float NaN to integer
-
-Case NaN 2:
-
-    >>> add_integer(float('nan'))
-    Traceback (most recent call last):
-    	      ...
-    ValueError: cannot convert float NaN to integer
+    Raises:
+        TypeError: If either of the arguments not an integer or a float
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
