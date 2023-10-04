288e85d7b309c7b6a256292227b05266d6e93578
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-10-04 23:30:35 +0300

    test

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A module for working with rectangles.
+"""
+
+
+class Rectangle:
+    """Represents a 2D Polygon with 4 perpendicular sides.
+    """
+    def __init__(self, width=0, height=0):
