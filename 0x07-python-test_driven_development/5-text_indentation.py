f36fe4d6b2de9d7acc588a41c5fb60c4089fa0fd
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-10-04 02:34:16 -0700

    dejfb

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,36 +1,24 @@
 #!/usr/bin/python3
 """
-
-This module contains a function that indents texts
-
+Function to replace some characters with '\n\n'
 """
 
 
 def text_indentation(text):
-    '''This function prints a text with 2 new lines after each ".", "?", or ":"
+    """
+    Prints a text with 2 new lines after some characters.
+    """
 
-    Args:
-        text (str): The string to be printed
-
-    Raises:
-        TypeError: If text is not a string
-
-    '''
-
-    if not isinstance(text, str):
+    if type(text) is not str:
         raise TypeError("text must be a string")
-
-    count = 0
-    while count < len(text) and text[count] == " ":
-        count = count + 1
-
-    while count < len(text):
-        print(text[count], end="")
-        if text[count] == "\n" or text[count] in ".?:":
-            if text[count] in ".?:":
-                print("\n")
-            count = count + 1
-            while count < len(text) and text[count] == " ":
-                count = count + 1
-            continue
-        count = count + 1
+    tmp = text.replace(".", ".\n\n")
+    tmp = tmp.replace(":", ":\n\n")
+    tmp = tmp.replace("?", "?\n\n")
+    p = tmp.splitlines(True)
+    ls_strip = []
+    for l in p:
+        if l == "\n":
+            ls_strip.append("\n")
+        else:
+            ls_strip.append(l.lstrip())
+    print("".join(ls_strip), end="")
