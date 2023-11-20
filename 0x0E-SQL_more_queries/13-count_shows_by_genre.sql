c61c3d4ad21c393cf3ff64008a9f9f6566a717ce
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-11-20 03:40:29 +0200

    test

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,9 +2,10 @@
 -- shows linked to each.
 -- Does not display genres without linked shows.
 -- Records are ordered by descending number of shows linked.
-SELECT name as "genre", count(*) as "number_of_shows"
-FROM tv_genres tg
-LEFT JOIN tv_show_genres tsg
-ON tg.id = tsg.genre_id
-group by tg.name
-ORDER BY `number_of_shows` DESC;
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
