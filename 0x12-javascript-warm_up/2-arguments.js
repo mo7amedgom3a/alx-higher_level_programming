9819143a93e809a14f7d46923a0913bfe7887e5d
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-01-14 02:09:48 +0200

    test

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const argc = process.argv.length;
+
 if (argc > 2) {
-  console.log('Argument found');
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
   console.log('No argument');
 }
