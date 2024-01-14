2b7547b354518e0798afc1aa714eca863c33d219
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-01-14 03:10:17 +0200

    test

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@ if (isNaN(num)) {
   for (let i = 0; i < num; i++) {
     let line = '';
     for (let j = 0; j < num; j++) {
-      line += '#';
+      line += 'X';
     }
     console.log(line);
   }
