3d02c62b4c0678cab6d15bba57e6549e215692e8
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-02-14 00:17:45 +0200

    test

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myRow = '';
+      let y = 0;
+      while (y < this.width) {
+        myRow += 'X';
+        y++;
+      }
+      console.log(myRow);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
