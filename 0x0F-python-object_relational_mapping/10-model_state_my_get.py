235db2d1084bea5b35aa2468419baac71214136c
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-02-17 22:35:37 +0200

    test

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     session = Session()
 
-    state = session.query(State).filter(State.name == 'Illinois').first()
+    state = session.query(State).filter(State.name == argv[4]).first()
     if state is not None:
         print('{}'.format(state.id))
     else:
