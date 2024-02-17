7caf9c74317e46841ea63b93915025dc6594934c
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-02-17 18:53:13 +0200

    test

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+#!/usr/bin/python3
+"""
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+rows = session.query(State).order_by(State.id)
+for row in rows:
+    print("{}: {}".format(row.id, row.name))
