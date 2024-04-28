6db4629e02231fee32d1df51b20260b0564146f8
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-04-29 01:11:04 +0300

    test

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const url = process.argv[2];
 const request = require('request');
 
-const character_ID = '18';
+const characterid = '18';
 let count = 0;
 
 request.get(url, (error, response, body) => {
@@ -13,7 +13,7 @@ request.get(url, (error, response, body) => {
     const films = JSON.parse(body).results;
     for (const filme of films) {
       for (const character of filme.characters) {
-        if (character.includes(character_ID)) {
+        if (character.includes(characterid)) {
           count++;
         }
       }
