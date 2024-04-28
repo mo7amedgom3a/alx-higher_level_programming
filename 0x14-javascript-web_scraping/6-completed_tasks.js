47ae41e40adb22c44e5c287256c0ef9616d21aa2
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-04-29 01:53:26 +0300

    test

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const tasks = JSON.parse(body);
+    const completed = {};
+    for (const task of tasks) {
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  }
+});
